82b78219bc763de00f0ae8ff9044b0a0c8bf874c
Delfina8 <githubdelfina@gmail.com>
2024-12-29 17:30:33 -0300

    Implementando-uma-Collection-Resource

---
+++ src/main/java/com/app/apptransito/api/controller/ProprietarioController.java
@@ -0,0 +1,32 @@
+package com.app.apptransito.api.controller;
+
+import com.app.apptransito.domain.model.Proprietario;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class ProprietarioController {
+
+    @GetMapping("/proprietarios")
+    public List<Proprietario> listar(){
+        var proprietario1 = new Proprietario();
+        Proprietario proprietario2 = new Proprietario();
+
+        proprietario1.setId(1L);
+        proprietario1.setNome("João");
+        proprietario1.setTelefone("34 99999-1111");
+        proprietario1.setEmail("joaodascouves@gmail.com");
+
+        proprietario2.setId(2L);
+        proprietario2.setNome("Maria");
+        proprietario2.setTelefone("11 97777-1111");
+        proprietario2.setEmail("mariadasplantas@gmail.com");
+
+        return Arrays.asList(proprietario1, proprietario2);
+    }
+
+
+}
---
+++ src/test/java/com/app/apptransito/TransitoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.apptransito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransitoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
