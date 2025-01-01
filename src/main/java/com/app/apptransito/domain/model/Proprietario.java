02eded71ccd910998c91f1949c47a71a1434d498
Delfina8 <githubdelfina@gmail.com>
2025-01-01 12:01:28 -0300

    Mapeando a entidade Proprietário com Jakarta Persistence

--- src/main/java/com/app/apptransito/domain/model/Proprietario.java
+++ src/main/java/com/app/apptransito/domain/model/Proprietario.java
@@ -1,12 +1,21 @@
 package com.app.apptransito.domain.model;
 
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+//@Table(name = "propietario")
 public class Proprietario {
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String nome;
     private String email;
     private String telefone;
