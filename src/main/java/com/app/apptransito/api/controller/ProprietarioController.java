9f48bac1070634d8a6d0c214c0979b9e84cdfa4b
Delfina8 <githubdelfina@gmail.com>
2025-01-05 16:10:26 -0300

    Criar e usar o repositório.

--- src/main/java/com/app/apptransito/api/controller/ProprietarioController.java
+++ src/main/java/com/app/apptransito/api/controller/ProprietarioController.java
@@ -1,8 +1,10 @@
 package com.app.apptransito.api.controller;
 
 import com.app.apptransito.domain.model.Proprietario;
+import com.app.apptransito.domain.repository.ProprietarioRepository;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,13 +13,18 @@ import java.util.List;
 @RestController
 public class ProprietarioController {
 
-    @PersistenceContext
-    private EntityManager entityManager;
+//    @Autowired
+//    private ProprietarioRepository proprietarioRepository;
+
+    private final ProprietarioRepository proprietarioRepository;
+
+    public ProprietarioController(ProprietarioRepository proprietarioRepository){
+        this.proprietarioRepository = proprietarioRepository;
+    }
 
     @GetMapping("/proprietarios")
     public List<Proprietario> listar(){
-        return entityManager.createQuery("from Proprietario", Proprietario.class)
-                .getResultList();
+        return proprietarioRepository.findAll();
     }
 
 
