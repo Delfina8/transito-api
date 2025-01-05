2a23d9b4d70b854b66e092d6296c6f08fc826bd6
Delfina8 <githubdelfina@gmail.com>
2025-01-05 18:20:54 -0300

    Implementando Query Methods no repositório.

--- src/main/java/com/app/apptransito/domain/repository/ProprietarioRepository.java
+++ src/main/java/com/app/apptransito/domain/repository/ProprietarioRepository.java
@@ -2,8 +2,14 @@ package com.app.apptransito.domain.repository;
 
 import com.app.apptransito.domain.model.Proprietario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ProprietarioRepository extends JpaRepository<Proprietario, Long> {
+    List<Proprietario> findByNome(String nome);
+
+    List<Proprietario> findAllByNomeContainingIgnoreCase(@Param("nome") String nome);
 }
