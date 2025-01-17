7d56ed315e0f9d05c362aa1391daf47e78fc6786
Delfina8 <githubdelfina@gmail.com>
2025-01-17 13:47:31 -0300

    Customização das informações do ProblemDetail.

--- src/main/java/com/app/apptransito/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/app/apptransito/api/exceptionhandler/ApiExceptionHandler.java
@@ -1,10 +1,17 @@
 package com.app.apptransito.api.exceptionhandler;
 
 import com.app.apptransito.domain.exception.NegocioException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.net.URI;
 
 @RestControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
@@ -13,5 +20,17 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
     public ResponseEntity<String> capturar(NegocioException e) {
         return ResponseEntity.badRequest().body(e.getMessage());
     }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers,
+                                                                  HttpStatusCode status, WebRequest request) {
+        ProblemDetail problemDetail = ProblemDetail.forStatus(status);
+        problemDetail.setTitle("Um ou mais campos estão inválidos");
+        problemDetail.setType(URI.create("https://transitoapi.com/erros/campos-invalidos"));
+
+        return this.handleExceptionInternal(ex, problemDetail, headers, status, request);
+    }
+
 
 }
